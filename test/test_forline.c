f9fa28a3aaf1a6ead8c107e7a52c2d2e196b3364
Deniz Yuret <denizyuret@gmail.com>
2014-01-13 21:44:06 +0200

    *** empty log message ***

--- test/test_forline.c
+++ test/test_forline.c
@@ -1,12 +1,11 @@
+#include <stdio.h>
 #include "dlib.h"
 
 int main(int argc, char **argv) {
   char *fname = (argc == 1) ? NULL : argv[1];
   msg("Reading %s", fname == NULL ? "stdin" : fname);
   forline(buf, fname) {
-    int len;
-    for (len = 0; buf[len] != 0; len++);
-    msg("%d", len);
+    fputs(buf, stdout);
   }
 }
 
