eecfcb03f5adbd201b677eb6bd14494d822e9632
Deniz Yuret <denizyuret@gmail.com>
2014-01-20 14:54:14 +0200

    New test cases.

--- test/test_fortok.c
+++ test/test_fortok.c
@@ -2,11 +2,27 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  char *s = strdup("   To be    or not");
-  fortok (tok, s) {
-    printf("%s:", tok); // prints To:be:or:not
+  char *str = strdup("  To be    or not ");
+  // need strdup because fortok won't work with constant strings
+  fortok (tok, str) {
+    printf("[%s]", tok); // prints "[To][be][or][not]"
   }
-  puts("");
+  putchar('\n');
+
+  char *pwd = strdup(":root::/root:/bin/bash:");
+  fortok3 (tok, pwd, ":/") {
+    printf("[%s]", tok); // prints "[root][root][bin][bash]"
+  }
+  putchar('\n');
+
+  char *pwd2 = strdup(":root::/root:/bin/bash:");
+  char **toks = malloc(10 * sizeof(char *));
+  int n = split(pwd2, ":/", toks, 10);
+  for (int i = 0; i < n; i++) {
+    printf("[%s]", toks[i]); // prints "[][root][][][root][][bin][bash][]"
+  }
+  putchar('\n');
+
   char *fname = (argc == 1) ? NULL : argv[1];
   msg("Reading %s", fname == NULL ? "stdin" : fname);
   forline(buf, fname) {
